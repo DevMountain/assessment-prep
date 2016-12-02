157e8cf9060180b7f995a4ded1c5f0c5b83335d4
rnoelle <reidnoelle2@gmail.com>
2016-12-02 15:47:43 -0700

    finished empty, ready to test

---
+++ basic-js-review/review.js
@@ -0,0 +1,55 @@
+/////////////Variables/////////////
+//1. Write a variable called color with a value of "dark blue".
+
+//2. Write a variable, myFave, that has the value "My favorite color is"
+
+//3. Use the variables from 1 and 2 to make myFavoriteColor equal "My favorite color is dark blue"
+var myFavoriteColor;
+
+//4. Create an array of strings that includes the letters a - d in order.
+var aThroughD;
+
+//5. Remove 'a' and store it to a variable called isForApple.
+
+//6. Add 'e' to the end of aThroughD.
+
+//7. Add 'apple' to the beginning of aThroughD
+
+//8. Remove 'c' from the array and put 'cat' in its place
+
+//9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
+
+//10. Create an object called spaceJam
+//    spaceJam has: a title of 'Space Jam' (String)
+//                  a release_year of 1996 (Number)
+//                  is not an oscarAwardWinner (Boolean)
+//                  and has the stars Michael Jordan, Bugs Bunny, and Bill Murray (array of strings)
+
+//11. Change anakin's side to dark using bracket notation
+
+var anakinSkywalker =  {
+  side: 'light',
+  other_names: ['Darth Vader', 'Ani'],
+  children: ['Luke', 'Leia'],
+  former_significant_other: 'Padme Amidala',
+  former_mother: 'Shmi Skywalker'
+}
+
+//12. Add 'Jar Jar' to his children using dot notation
+
+//13. Loop through the object and remove any property that starts with former
+
+//14. Create a function called finder that takes in an array and a string and loops through the array
+//    to remove every instance of that strings
+
+//15. Write a function called numChecker that takes in a parameter and checks to see if it is a number or a
+  //number in a string - if so, return true, else return false
+
+//16. Write a function called rightNow that returns a new date object
+
+//17. Write a function called whoAmI that takes in an object and returns the value of the
+//    object's name property, and if the object's name
+//    is undefined, return 'has amnesia'.
+
+//18. Write a function called returnMe that takes in a num and a callback function and
+//    returns the result of the callback function invoked with the num as a parameter
